9d7800acd080d23b77d5643ddeadb09cc1c8c626
Robin Gingras <robin@robingingras.com>
2016-06-09 21:00:10 -0700

    fix for django 1.9

--- django_settings/models.py
+++ django_settings/models.py
@@ -2,7 +2,7 @@
 # framework
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.utils.translation import ugettext_lazy as _
 from django.dispatch import receiver
 from django.db.models.signals import post_syncdb
@@ -61,7 +61,7 @@ class Setting(models.Model):
 
     setting_type = models.ForeignKey(ContentType)
     setting_id = models.PositiveIntegerField()
-    setting_object = generic.GenericForeignKey('setting_type', 'setting_id')
+    setting_object = GenericForeignKey('setting_type', 'setting_id')
 
     name = models.CharField(max_length=255, unique=conf.DJANGO_SETTINGS_UNIQUE_NAMES)
 
